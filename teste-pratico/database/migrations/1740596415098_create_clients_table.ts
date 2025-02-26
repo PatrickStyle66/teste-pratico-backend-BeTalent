97f936832d398063337b46a192db41e07ee0495e
PatrickStyle66 <jphamega@hotmail.com>
2025-02-26 16:25:30 -0300

    feat(Client): added client model and migration

---
+++ teste-pratico/database/migrations/1740596415098_create_clients_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'clients'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table
+        .string('email')
+        .notNullable()
+        .references('email')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
