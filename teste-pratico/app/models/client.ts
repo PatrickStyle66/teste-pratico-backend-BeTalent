cd61d98a80574c508c7ca9b8650c702e18cd745f
PatrickStyle66 <jphamega@hotmail.com>
2025-02-27 15:08:59 -0300

    refactor(Clients): added relationship with transactions

--- teste-pratico/app/models/client.ts
+++ teste-pratico/app/models/client.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import Transaction from './transaction.js'
+import { type HasMany } from '@adonisjs/lucid/types/relations'
 
 export default class Client extends BaseModel {
   @column({ isPrimary: true })
@@ -10,6 +12,9 @@ export default class Client extends BaseModel {
 
   @column()
   declare email: string
+
+  @hasMany(() => Transaction)
+  declare transaction: HasMany<typeof Transaction>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
