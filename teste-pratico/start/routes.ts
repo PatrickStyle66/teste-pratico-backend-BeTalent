a062c73f47aa8c679f68cb5116a9220e7d79c761
PatrickStyle66 <jphamega@hotmail.com>
2025-03-02 21:35:27 -0300

    feat(login): Added login route and controller

--- teste-pratico/start/routes.ts
+++ teste-pratico/start/routes.ts
@@ -1,13 +1,5 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 const UsersController = () => import('#controllers/users_controller')
+const SessionController = () => import('#controllers/session_controller')
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
@@ -15,4 +7,5 @@ router.get('/', async () => {
     hello: 'world',
   }
 })
+router.post('login', [SessionController, 'store'])
 router.resource('user', UsersController).apiOnly()
