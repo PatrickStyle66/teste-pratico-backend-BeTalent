194eab2bd8d05515d26017bd04e300feb4e824d7
PatrickStyle66 <jphamega@hotmail.com>
2025-03-02 21:12:05 -0300

    feat(Validator):Added users validator

--- teste-pratico/app/controllers/users_controller.ts
+++ teste-pratico/app/controllers/users_controller.ts
@@ -1,4 +1,5 @@
 import User from '#models/user'
+import { createUserValidator } from '#validators/user'
 import type { HttpContext } from '@adonisjs/core/http'
 export default class UsersController {
   /**
@@ -10,7 +11,7 @@ export default class UsersController {
    * Display form to create a new record
    */
   async store({ request }: HttpContext) {
-    const { name, email, password } = request.body()
+    const { name, email, password } = await request.validateUsing(createUserValidator)
     const user = await User.create({ email, password })
     await user.related('client').create({ name, email })
     return user
