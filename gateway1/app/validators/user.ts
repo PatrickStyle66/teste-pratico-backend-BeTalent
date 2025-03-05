4370dc246d6f932a0656b05b43b01d2c0ac45a9f
PatrickStyle66 <jphamega@hotmail.com>
2025-03-05 14:49:37 -0300

    feat(validator): added optional values to update user validator

--- gateway1/app/validators/user.ts
+++ gateway1/app/validators/user.ts
@@ -17,7 +17,6 @@ export const createUserValidator = vine.compile(
 
 export const updateUserValidator = vine.compile(
   vine.object({
-    name: vine.string().trim(),
     email: vine
       .string()
       .email()
@@ -25,7 +24,8 @@ export const updateUserValidator = vine.compile(
       .unique(async (db, value) => {
         const match = await db.from('users').select('id').where('email', value).first()
         return !match
-      }),
-    password: vine.string().minLength(6),
+      })
+      .optional(),
+    password: vine.string().minLength(6).optional(),
   })
 )
