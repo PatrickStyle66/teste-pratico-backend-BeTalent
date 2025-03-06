19a90a3e54d1f35d7365e1249a10d56a46da0008
PatrickStyle66 <jphamega@hotmail.com>
2025-03-06 16:11:18 -0300

    feat(Routes):Added product routes

--- gateway1/start/routes.ts
+++ gateway1/start/routes.ts
@@ -2,6 +2,7 @@ const UsersController = () => import('#controllers/users_controller')
 const SessionController = () => import('#controllers/session_controller')
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+const ProductsController = () => import('#controllers/products_controller')
 
 router.get('/', async () => {
   return {
@@ -16,5 +17,6 @@ router
     router.put('user/:id', [UsersController, 'update'])
     router.delete('user/:id', [UsersController, 'destroy'])
     router.get('user', [UsersController, 'index'])
+    router.resource('product', ProductsController)
   })
   .use(middleware.auth())
