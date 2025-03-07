762452414e66777aae081dbc2c12d1311f48cb7c
PatrickStyle66 <jphamega@hotmail.com>
2025-03-07 16:00:28 -0300

    feat(Gateway): added gateway controller

--- gateway1/app/controllers/gateways_controller.ts
+++ gateway1/app/controllers/gateways_controller.ts
@@ -1,15 +1,23 @@
+import Gateway from '#models/gateway'
+import { createGatewayValidator } from '#validators/gateway'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class GatewaysController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({}: HttpContext) {
+    const gateways = await Gateway.query()
+    return gateways
+  }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request }: HttpContext) {
+    const { name, isActive, priority } = await request.validateUsing(createGatewayValidator)
+    const gateway = await Gateway.create({ name, isActive, priority })
+  }
 
   /**
    * Show individual record
