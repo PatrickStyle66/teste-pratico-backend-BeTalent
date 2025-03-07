e5449bd97aad4f34abba61693e711478993780ac
Jonathas Patrick <jphamega@hotmail.com>
2025-03-07 18:49:25 -0300

    feat(gateway):Added gateways functions

--- gateway1/app/controllers/gateways_controller.ts
+++ gateway1/app/controllers/gateways_controller.ts
@@ -1,5 +1,5 @@
 import Gateway from '#models/gateway'
-import { createGatewayValidator } from '#validators/gateway'
+import { createGatewayValidator, updateGatewayValidator } from '#validators/gateway'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class GatewaysController {
@@ -15,22 +15,47 @@ export default class GatewaysController {
    * Handle form submission for the create action
    */
   async store({ request }: HttpContext) {
-    const { name, isActive, priority } = await request.validateUsing(createGatewayValidator)
-    const gateway = await Gateway.create({ name, isActive, priority })
+    const { name, priority } = await request.validateUsing(createGatewayValidator)
+    const gateway = await Gateway.create({ name, priority })
+    return gateway
   }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params, response }: HttpContext) {
+    try {
+      const gateway = await Gateway.findByOrFail('id', params.id)
+      return gateway
+    } catch (error) {
+      return response.status(400).json('gateway not found')
+    }
+  }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    try {
+      const gateway = await Gateway.findByOrFail('id', params.id)
+      const { name, priority } = await request.validateUsing(updateGatewayValidator)
+      gateway!.merge({ name, priority })
+      await gateway!.save()
+    } catch (error) {
+      return response.status(400).json('gateway not found')
+    }
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, response }: HttpContext) {
+    try {
+      const gateway = await Gateway.findByOrFail('id', params.id)
+      await gateway!.delete()
+      return response.status(203)
+    } catch (error) {
+      return response.status(400).json('gateway not found')
+    }
+  }
 }
