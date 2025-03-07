3b4f84e54dde285e2f0039aaddc33576640984b0
PatrickStyle66 <jphamega@hotmail.com>
2025-03-07 11:35:48 -0300

    fix(Migrations): removed nullable property of role

--- gateway1/database/migrations/1740581880130_create_users_table.ts
+++ gateway1/database/migrations/1740581880130_create_users_table.ts
@@ -8,7 +8,7 @@ export default class extends BaseSchema {
       table.increments('id').notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
-      table.string('role').nullable().defaultTo('user')
+      table.string('role').defaultTo('user')
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
