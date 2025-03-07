c320f8c2dfb6b132d1aca2a3df63f14ab7b30aae
Jonathas Patrick <jphamega@hotmail.com>
2025-03-07 18:48:30 -0300

    feat(validator): Added update gateway validator

--- gateway1/app/validators/gateway.ts
+++ gateway1/app/validators/gateway.ts
@@ -2,7 +2,13 @@ import vine from '@vinejs/vine'
 export const createGatewayValidator = vine.compile(
   vine.object({
     name: vine.string(),
-    isActive: vine.boolean(),
     priority: vine.number(),
+  })
+)
+
+export const updateGatewayValidator = vine.compile(
+  vine.object({
+    name: vine.string().optional(),
+    priority: vine.number().optional(),
   })
 )
