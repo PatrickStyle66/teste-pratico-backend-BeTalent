2d7a6885a007c28471694b2d0b9ca869cf8c62b2
Jonathas Patrick <jphamega@hotmail.com>
2025-03-07 18:45:50 -0300

    refactor(gateways): changed is_active field default value to true

--- gateway1/database/migrations/1740678440411_create_gateways_table.ts
+++ gateway1/database/migrations/1740678440411_create_gateways_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('name').notNullable()
-      table.boolean('is_active').notNullable()
+      table.boolean('is_active').notNullable().defaultTo(true)
       table.integer('priority').notNullable()
       table.timestamp('created_at')
       table.timestamp('updated_at')
