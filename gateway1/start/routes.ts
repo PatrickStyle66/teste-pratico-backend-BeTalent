713e7b753dc037efd0db204eb54a193ba4c68de6
PatrickStyle66 <jphamega@hotmail.com>
2025-03-09 19:55:50 -0300

    feat(routes):added transaction routes

--- gateway1/start/routes.ts
+++ gateway1/start/routes.ts
@@ -2,6 +2,7 @@ const UsersController = () => import('#controllers/users_controller')
 const SessionController = () => import('#controllers/session_controller')
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+const TransactionsController = () => import('#controllers/transactions_controller')
 const GatewaysController = () => import('#controllers/gateways_controller')
 const ProductsController = () => import('#controllers/products_controller')
 
@@ -12,6 +13,7 @@ router.get('/', async () => {
 })
 router.post('login', [SessionController, 'store'])
 router.post('user', [UsersController, 'store'])
+router.post('transactions', [TransactionsController, 'store'])
 router
   .group(() => {
     router.get('user/:id', [UsersController, 'show'])
@@ -21,5 +23,6 @@ router
     router.resource('product', ProductsController)
     router.put('user/:id/role', [UsersController, 'role'])
     router.resource('gateway', GatewaysController)
+    router.get('transactions', [TransactionsController, 'index'])
   })
   .use(middleware.auth())
